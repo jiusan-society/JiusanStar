733b295923b534ea5c7066d92e4fa5078f4c2cd6
Marcus Lin <linfaimom@gmail.com>
2017-10-15 17:43:26 +0800

    add user entity and role entity

--- src/main/java/gov/jiusan/star/score/Score.java
+++ src/main/java/gov/jiusan/star/score/Score.java
@@ -14,7 +14,7 @@ import java.util.Calendar;
  * @author Marcus Lin
  */
 @Entity
-@Table(name = "score")
+@Table(name = "org_score")
 @NamedQueries({
     @NamedQuery(name = "Score.selectAll", query = "select s from Score s")
 })
---
+++ src/main/java/gov/jiusan/star/user/User.java
@@ -0,0 +1,89 @@
+package gov.jiusan.star.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author Marcus Lin
+ */
+@Entity
+@Table(name = "app_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seq", nullable = false)
+    private Long seq;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "tel")
+    private String tel;
+
+    @ManyToOne
+    private Role role;
+
+    public User() {
+    }
+
+    public Long getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Long seq) {
+        this.seq = seq;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+}
