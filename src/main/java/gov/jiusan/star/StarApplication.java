96a25dc764ef81026482e8a1418be6509d86c247
Marcus Lin <marcus.lin@3medu.com>
2017-12-23 11:33:34 +0800

    role、user 的实体需重新设计；html 添加 bootstrap.min.js

--- src/main/java/gov/jiusan/star/StarApplication.java
+++ src/main/java/gov/jiusan/star/StarApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(StarApplication.class, args);
     }
 }
