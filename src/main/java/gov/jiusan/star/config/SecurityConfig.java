c34aef0e7ab96c92e9bbadd336aaa7b8a406f1ff
Marcus Lin <linfaimom@gmail.com>
2018-03-23 22:02:24 +0800

    不同角色跳转进入不同页面

--- src/main/java/gov/jiusan/star/config/SecurityConfig.java
+++ src/main/java/gov/jiusan/star/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package gov.jiusan.star.config;
 
+import gov.jiusan.star.redirect.RedirectComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,6 +17,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final RedirectComponent redirectBean;
+
+    @Autowired
+    public SecurityConfig(RedirectComponent redirectBean) {
+        this.redirectBean = redirectBean;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -23,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .formLogin()
                 .loginPage("/login").permitAll()
+            .successHandler(redirectBean)
             .and()
             .logout();
     }
