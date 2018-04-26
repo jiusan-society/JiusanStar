dd1afe118b94cc7437093b8dc374d1c985d4dfaa
Marcus Lin <marcus.lin@3medu.com>
2018-04-26 09:22:12 +0800

    修复错误的实体关系

--- src/main/java/gov/jiusan/star/sheet/RatingSheetController.java
+++ src/main/java/gov/jiusan/star/sheet/RatingSheetController.java
@@ -1,5 +1,6 @@
 package gov.jiusan.star.sheet;
 
+import gov.jiusan.star.org.Org;
 import gov.jiusan.star.org.OrgService;
 import gov.jiusan.star.sheet.model.RatingDetails;
 import gov.jiusan.star.sheet.model.RatingPhase;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -150,7 +152,11 @@ public class RatingSheetController {
         if (!sheet.isPresent()) {
             return "error";
         }
-        rsService.dispatchSheet(sheet.get(), oService.findAllNonRootOrgs());
+        List<Org> orgs = oService.findAllNonRootOrgs();
+        if (orgs.isEmpty()) {
+            return "error";
+        }
+        rsService.dispatchSheet(sheet.get(), orgs);
         return "sheet/sheet_list";
     }
 
