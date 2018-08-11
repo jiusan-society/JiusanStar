ebd9d2d7924d43ee5dce4f1394345a0edbbe6b3d
Marcus Lin <marcus.lin@3medu.com>
2018-08-12 00:21:20 +0800

    Format & 美化界面

--- src/main/java/gov/jiusan/star/user/UserRepository.java
+++ src/main/java/gov/jiusan/star/user/UserRepository.java
@@ -1,7 +1,6 @@
 package gov.jiusan.star.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
