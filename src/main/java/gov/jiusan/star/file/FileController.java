23012cf4601a97c3bbbce669392739b3d35bfbc7
Marcus Lin <linfaimom@gmail.com>
2019-01-01 13:42:44 +0800

    更新 copyright & author

--- src/main/java/gov/jiusan/star/file/FileController.java
+++ src/main/java/gov/jiusan/star/file/FileController.java
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2019 Marcus Lin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gov.jiusan.star.file;
 
-import gov.jiusan.star.user.LoggedUser;
 import gov.jiusan.star.org.Org;
 import gov.jiusan.star.org.OrgService;
 import gov.jiusan.star.org.OrgUtil;
 import gov.jiusan.star.org.model.OrgDTO;
 import gov.jiusan.star.user.CustomUserDetails;
+import gov.jiusan.star.user.LoggedUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
--- src/main/java/gov/jiusan/star/user/UserUtil.java
+++ src/main/java/gov/jiusan/star/user/UserUtil.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2019 Marcus Lin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gov.jiusan.star.user;
 
 import gov.jiusan.star.user.model.Profile;
 
-public class UserUtil {
+/**
+ * @author Marcus Lin
+ */
+class UserUtil {
 
     static Profile convertToModel(User user) {
         Profile p = new Profile();
